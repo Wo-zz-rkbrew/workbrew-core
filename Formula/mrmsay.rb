b1ea18c15a2cd2c8107635ef07cb7a07ec3ad121
Jane Appleseed <janeappleseedgh@gmail.com>
2016-10-17 16:45:41 -0400

    mrmsay: add launchd plist

--- Formula/mrmsay.rb
+++ Formula/mrmsay.rb
@@ -49,6 +49,29 @@ class Mrmsay < Formula
     virtualenv_install_with_resources
   end
 
+  plist_options :startup => true, :manual => "mrmd"
+
+  def plist; <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN"
+    "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>Program</key>
+        <string>#{opt_bin}/mrmd</string>
+        <key>KeepAlive</key>
+        <true/>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>ProcessType</key>
+        <string>Background</string>
+      </dict>
+    </plist>
+    EOS
+  end
+
   test do
     ENV["XDG_CONFIG_HOME"] = testpath/".config"
     ENV["XDG_CACHE_HOME"] = testpath/".cache"
